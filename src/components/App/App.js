0a0b4305bc9ed812452059cc63e883e9ae88586e
Michael Michaud <mmichaud3@yahoo.com>
2022-03-15 11:38:35 -0700

    icons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,10 @@
 import MainContent from '../../MainContent/MainContent';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
+library.add(faLinkedinIn);
 
 function App() {
   return (
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer() {
   return (
     <div className='footer'>
-      <div className='footer__socials'>footer</div>
+      <div className='footer__socials'>
+        <FontAwesomeIcon icon='fa-brands fa-linkedin-in' />
+      </div>
       <div className='footer__email'>
         <a href='mailto:mvmichaud3@gmail.com'>mvmichaud3@gmail.com</a>
       </div>
